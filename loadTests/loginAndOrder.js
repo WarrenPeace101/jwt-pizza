a54bdc5d282bc37f1a675af7067551ffe39f6b1e
WarrenPeace101 <mangum.warren99@gmail.com>
2024-11-20 16:49:28 -0700

    added new load test

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,171 @@
+import { sleep, check, group, fail } from 'k6'
+import http from 'k6/http'
+
+export const options = {
+  cloud: {
+    distribution: { 'amazon:us:ashburn': { loadZone: 'amazon:us:ashburn', percent: 100 } },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 15, duration: '1m' },
+        { target: 10, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      gracefulRampDown: '30s',
+      exec: 'scenario_1',
+    },
+  },
+}
+
+export function scenario_1() {
+  let response
+
+  group('page_1 - https://pizza.warrendeployment.click/', function () {
+    // Homepage
+    response = http.get('https://pizza.warrendeployment.click/', {
+      headers: {
+        accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'cache-control': 'max-age=0',
+        'if-modified-since': 'Fri, 01 Nov 2024 00:56:13 GMT',
+        'if-none-match': '"c558649abed6d38b060462a25e7be698"',
+        priority: 'u=0, i',
+        'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+      },
+    })
+    sleep(17.1)
+
+    // Login
+    response = http.put(
+      'https://pizza-service.warrendeployment.click/api/auth',
+      '{"email":"mangum.warren99@gmail.com","password":"123"}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          origin: 'https://pizza.warrendeployment.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    if (!check(response, { 'status equals 200': response => response.status.toString() === '200' })) {
+      console.log(response.body);
+      fail('Login was *not* 200');
+    }
+    sleep(7.9)
+
+    // Get menu
+    response = http.get('https://pizza-service.warrendeployment.click/api/order/menu', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        'if-none-match': 'W/"f2-b7f7TZAQ695lkLkh79C97fy7vgs"',
+        origin: 'https://pizza.warrendeployment.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+
+    // Get franchise
+    response = http.get('https://pizza-service.warrendeployment.click/api/franchise', {
+      headers: {
+        accept: '*/*',
+        'accept-encoding': 'gzip, deflate, br, zstd',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/json',
+        'if-none-match': 'W/"6f-tkBaqR+idvK+KNmqg1cESbwYUqY"',
+        origin: 'https://pizza.warrendeployment.click',
+        priority: 'u=1, i',
+        'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+      },
+    })
+    sleep(11.4)
+
+    // Purchase pizza
+    let purchaseResponse
+    purchaseResponse = http.post(
+      'https://pizza-service.warrendeployment.click/api/order',
+      '{"items":[{"menuId":2,"description":"ChocolateDeluxe","price":0.099}],"storeId":"2","franchiseId":1}',
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          origin: 'https://pizza.warrendeployment.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-site',
+        },
+      }
+    )
+    if (!check(purchaseResponse, { 'status equals 200': purchaseResponse => purchaseResponse.status.toString() === '200' })) {
+      console.log(purchaseResponse.body);
+      fail('Purchase was *not* 200');
+    }
+    sleep(2.3)
+    console.log(purchaseResponse.body.valueOf.arguments.jwt);
+    let jwtID = purchaseResponse.body.valueOf.arguments.jwt; 
+
+    // Verify pizza
+    response = http.post(
+      'https://pizza-factory.cs329.click/api/order/verify',
+      `{"jwt": ${jwtID}}`,
+      {
+        headers: {
+          accept: '*/*',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'en-US,en;q=0.9',
+          'content-type': 'application/json',
+          origin: 'https://pizza.warrendeployment.click',
+          priority: 'u=1, i',
+          'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'cross-site',
+        },
+      }
+    )
+  })
+}
